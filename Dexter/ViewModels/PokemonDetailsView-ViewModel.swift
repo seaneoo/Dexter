516d2cc736e7992fafae6f9ecbfc1766e05dd9fd
Sean O'Connor <so@seano.dev>
2023-07-24 09:08:29 -0600

    Create PokemonDetailsView and ViewModel

--- Dexter/ViewModels/PokemonDetailsView-ViewModel.swift
+++ Dexter/ViewModels/PokemonDetailsView-ViewModel.swift
@@ -6,3 +6,31 @@
 //
 
 import Foundation
+
+extension PokemonDetailsView {
+    @MainActor class ViewModel: ObservableObject {
+        @Published public var species: PokemonSpecies?
+
+        private var id: Int
+
+        init(id: Int) {
+            self.id = id
+        }
+
+        func fetchData() {
+            PokeAPI.shared.fetchPokemonSpecies(id: id) { [weak self] result in
+                switch result {
+                case let .success(data):
+                    DispatchQueue.main.async {
+                        self?.species = data
+                    }
+                case let .failure(error):
+                    DispatchQueue.main.async {
+                        self?.species = nil
+                        print(String(describing: error))
+                    }
+                }
+            }
+        }
+    }
+}
--- Dexter/Views/PokemonDetailsView.swift
+++ Dexter/Views/PokemonDetailsView.swift
@@ -8,13 +8,31 @@
 import SwiftUI
 
 struct PokemonDetailsView: View {
+    private var id: Int
+
+    @StateObject private var vm: ViewModel
+
+    init(id: Int) {
+        self.id = id
+        _vm = StateObject(wrappedValue: ViewModel(id: id))
+    }
+
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack {
+            if let species = vm.species {
+                Text(species.name)
+            } else {
+                ProgressView()
+            }
+        }
+        .task {
+            vm.fetchData()
+        }
     }
 }
 
 struct PokemonDetailsView_Previews: PreviewProvider {
     static var previews: some View {
-        PokemonDetailsView()
+        PokemonDetailsView(id: 25)
     }
 }
