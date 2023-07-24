c1b84dd1a9e6690652a59d1ae9173d2f222d5e8d
Sean O'Connor <so@seano.dev>
2023-07-23 22:36:04 -0600

    Create Pokedex and PokemonEntry types

---
+++ Dexter/API/Models/Games.swift
@@ -0,0 +1,20 @@
+//
+//  Games.swift
+//  Dexter
+//
+//  Created by Sean O'Connor on 7/23/23.
+//
+
+import Foundation
+
+struct Pokedex: Codable, Identifiable {
+    var id: Int
+    var name: String
+    var names: [Name]
+    var pokemonEntries: [PokemonEntry]
+}
+
+struct PokemonEntry: Codable {
+    var entryNumber: Int
+    var pokemonSpecies: NamedAPIResource
+}
