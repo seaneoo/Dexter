f780542d472414282ea333f8d7d4120eccf1d3d8
Sean O'Connor <so@seano.dev>
2023-07-24 18:11:44 -0600

    Begin project restructure

---
+++ Dexter/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Dexter
+//
+//  Created by Sean O'Connor on 7/20/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        NavigationStack {
+            VStack {
+            }
+            .navigationTitle("Dexter")
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
