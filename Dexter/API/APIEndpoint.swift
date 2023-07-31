f8951400fdd9cbc28c0922235fcc738aea5919c1
Sean O'Connor <so@seano.dev>
2023-07-31 11:10:41 -0600

    Create APIEndpoint enum

---
+++ Dexter/API/APIEndpoint.swift
@@ -0,0 +1,13 @@
+//
+//  APIEndpoint.swift
+//  Dexter
+//
+//  Created by Sean O'Connor on 7/31/23.
+//
+
+import Foundation
+
+enum APIEndpoint: String {
+    case pokemonSpecies = "pokemon-species",
+         pokemon
+}
